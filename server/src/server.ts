80b6a5d6218e5c801301c0c2e98da4c5c67a3cc2
Sankha-Suvra <sankhasuvhraghatak@gmail.com>
2025-05-02 15:24:50 +0530

    fix origin url

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors({
-    origin: "http://localhost:5173", //https://hilarious-pie-38e44f.netlify.app
+    origin: "https://hilarious-pie-38e44f.netlify.app", //https://hilarious-pie-38e44f.netlify.app
     credentials: true,
 }))
 
